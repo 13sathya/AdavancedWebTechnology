2b10663c1cb2bd41d2cf732e893ff4d6c859a27f
Sathya Priya <116049935+13sathya@users.noreply.github.com>
2024-02-22 11:49:41 +0530

    Add files via upload

---
+++ Constructor.js
@@ -0,0 +1,31 @@
+class person{
+        constructor(){
+            this.name="Sathya";
+        } 
+        display(){
+            console.log(this.name);
+        }   
+}
+const persons=new person();
+persons.display();
+// class human{
+//     constructor(){
+//         this.age=20;
+//     }
+//     displayAge(){
+//         console.log(this.age);
+//     }
+// }
+//     class person extends human{
+//         constructor(){
+//             super();
+//             this.name="sathya";
+//             this.age=21;
+//         }
+//         displayName(){
+//             console.log(this.name);
+//         }
+//     }
+//     const p1=new person();
+//     p1.displayAge();
+//     p1.displayName();
